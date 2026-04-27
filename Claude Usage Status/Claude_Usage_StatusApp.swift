5e077fb8dff465005465f38326916be2f0bb287b
Drunken Whaler <13th.drunken.whaler@gmail.com>
2026-04-26 23:44:00 -0400

    Initial Commit

---
+++ Claude Usage Status/Claude_Usage_StatusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Claude_Usage_StatusApp.swift
+//  Claude Usage Status
+//
+//  Created by Whaler on 4/26/26.
+//
+
+import SwiftUI
+
+@main
+struct Claude_Usage_StatusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Claude Usage StatusTests/Claude_Usage_StatusTests.swift
@@ -0,0 +1,19 @@
+//
+//  Claude_Usage_StatusTests.swift
+//  Claude Usage StatusTests
+//
+//  Created by Whaler on 4/26/26.
+//
+
+import Testing
+@testable import Claude_Usage_Status
+
+struct Claude_Usage_StatusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
